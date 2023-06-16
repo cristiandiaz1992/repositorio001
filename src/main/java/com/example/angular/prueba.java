6bd05990cb66b94b276b7e4fbe03ba0fdc618e2e
cristian <cristian@DESKTOP-QIH8T5I>
2023-06-15 21:25:18 -0400

    segundo commit

---
+++ src/main/java/com/example/angular/prueba.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.angular;
+
+/**
+ *
+ * @author cristian
+ */
+public class prueba {
+    public static void main(String[] args) {
+        System.out.println("com.example.angular.prueba.main()");
+    }
+}
